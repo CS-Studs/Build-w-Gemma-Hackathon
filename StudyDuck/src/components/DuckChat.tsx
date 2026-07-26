569dcf08d1fa9308d42bbf4204532e30b861022e
Pheothyn <u10ha23@abdn.ac.uk>
2026-07-26 12:22:32 +0100

    67

---
+++ StudyDuck/src/components/DuckChat.tsx
@@ -0,0 +1,5 @@
+import './DuckChat.css';
+
+export function DuckChat() {
+  return <div></div>;
+}
--- StudyDuck/src/components/Workspace.tsx
+++ StudyDuck/src/components/Workspace.tsx
@@ -1,5 +1,6 @@
-import { DuckButton } from "./DuckButton";
-import "./Workspace.css";
+import { DuckButton } from './DuckButton';
+import { DuckChat } from './DuckChat';
+import './Workspace.css';
 
 /**
  * The normal app window: three columns, the last one split into two rows with
@@ -10,7 +11,9 @@ export function Workspace() {
   return (
     <main className="workspace">
       <section className="workspace__column" />
-      <section className="workspace__column" />
+      <section className="workspace__column">
+        <DuckChat />
+      </section>
       <section className="workspace__column workspace__column--rows">
         <div className="workspace__pane" />
         <div className="workspace__pane workspace__pane--duck">
